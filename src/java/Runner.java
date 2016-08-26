6c85c8c5674c5f542cec0da89f8c2d5f21a31e66
test <test@users-MacBook-Air.local>
2016-08-26 18:35:21 +0300

    added team part of the query

--- src/java/Runner.java
+++ src/java/Runner.java
@@ -1,9 +1,12 @@
 
+import static com.company.generators.TeamGenerator.generateListOfTeams;
+import com.company.tables.Team;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 
 
@@ -14,7 +17,25 @@ public class Runner {
         String url = "jdbc:derby://localhost:1527/SwissquoteTeamDB";
         String login = "root";
         String password = "";
-                
+        
+        List<Team> teams = generateListOfTeams(40, 2, 100, 4, 7, 4, 10);
+        
+        String teamsPart = new String();
+        for(Team team : teams) {
+            StringBuilder teamData = new StringBuilder();
+            teamData.append('(')
+                    .append(team.getId())
+                    .append(',')
+                    .append(team.getTeamName())
+                    .append(',')
+                    .append(team.getNumberOfMembers())
+                    .append(')');
+            teamsPart = teamsPart + new String(teamData);
+        }
+        
+        System.out.println(teamsPart);
+        
+/*        
         Connection connection = DriverManager.getConnection(url, login, password);
         Statement testStatement = connection.createStatement();
         ResultSet result = testStatement.executeQuery("select count(*) as total from teams");
@@ -22,7 +43,7 @@ public class Runner {
         while(result.next()) {
             System.out.println("output:" + result.getInt("total"));
         }
-        
+  */      
           
     }
     
